6b1286102ff2978d7c34ac353b56d262dd5636d8
Tludi <tludi00@gmail.com>
2014-05-22 22:44:01 -0700

    added iris and hybridizer list to admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,13 +3,33 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
+    columns do
+      column do
+        panel "Irises" do
+          ul do
+            Irisplant.order(name: :asc).map do |iris|
+              li link_to(iris.name, admin_irisplant_path(iris))
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Hybridizers" do
+          Hybridizer.order(lastname: :asc).map do |h|
+            li link_to(h.lastname + ", " + h.firstname, admin_hybridizer_path(h))
+          end
+        end
+      end
+
+    end
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
